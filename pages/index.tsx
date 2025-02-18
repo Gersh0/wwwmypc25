354c3aabf3169b5792cada748964a1b5647e5ccb
Gersh0 <german.rivera@eia.edu.co>
2025-02-17 20:30:01 -0500

    PruebaBoton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import { Title } from '@/components/atomic-design/atoms/texts/title';
+import Link from 'next/link';
 
 const Home = () => {
   return (
     <div className='h-full w-full'>
       <Title>Home</Title>
+      <Link href='/page-2'>
+        <a>Go to Page 2</a>
+      </Link>
     </div>
   );
 };
