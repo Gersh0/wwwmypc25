73e492051de01d1f56dbfc6928dd938a4a0e9762
vrtx <jose.gomez4@eia.edu.co>
2025-02-19 07:15:52 -0500

    about home section in progress

--- layouts/homeLayouts/AboutLayout.tsx
+++ layouts/homeLayouts/AboutLayout.tsx
@@ -5,25 +5,42 @@ import { ItemText } from '@/components/atomic-design/atoms/texts/itemText';
 
 const AboutLayout = () => {
   return (
-    <section className="min-h-screen w-full flex flex-col relative">
-
-      <TitleHome>
-        <div className="text-center text-4xl">
-          <p>Unlock the full</p>
-          <p>potential of your PC</p>
+    <section className="pr-32 pl-32 min-h-screen w-full flex flex-col relative">
+      <div className="flex text-center items-center text-4xl max-w-full">
+        <div className="max-w-96 mx-auto">
+          <TitleHome>
+            <p>Unlock the full potential of your PC</p>
+          </TitleHome>
         </div>
-      </TitleHome>
-
-  <div></div>
-      <div className="grid grid-cols-3 gap-4 mt-8">
-        <div className="w-[30%] h-64">
-          <TitleHome>Streamlined Diagnostics</TitleHome>
-          <ItemText >
-            <p className="text-balance">Our advanced diagnostic tools analyze your system's hardware and software components</p>
+      </div>
+      <div className="grid grid-cols-1 gap-4 mt-8 xl:grid-cols-3 ">
+        <div className="w-[100%] h-64">
+          <div className="text-2xl">
+            <TitleHome>Streamlined Diagnostics</TitleHome>
+          </div>
+          <ItemText>
+            <p className="text-balance text-3xs text-[#898786]">Our advanced diagnostic tools analyze your system's
+              hardware and software components</p>
           </ItemText>
         </div>
-        <div>Item 2</div>
-        <div>Item 3</div>
+        <div className="w-[100%] h-64">
+          <div className="text-2xl">
+            <TitleHome>Eficient Troubleshooting</TitleHome>
+          </div>
+          <ItemText>
+            <p className="text-balance text-3xs text-[#898786]">With our user-friendly chatbot, you can easily
+              communicate your concerns and receive tailored recommendations to resolve your PC </p>
+          </ItemText>
+        </div>
+        <div className="w-[100%] h-64">
+          <div className="text-2xl">
+            <TitleHome>Comprehensive Solutions</TitleHome>
+          </div>
+          <ItemText>
+            <p className="text-balance text-3xs text-[#898786]">Whether it's a hardware malfunction, software conflict,
+              or performance bottleneck </p>
+          </ItemText>
+        </div>
       </div>
     </section>
   );
--- layouts/homeLayouts/HeroLayout.tsx
+++ layouts/homeLayouts/HeroLayout.tsx
@@ -1,39 +1,55 @@
+// `layouts/homeLayouts/HeroLayout.tsx`
+import { useRouter } from 'next/router';
 import { TitleHome } from '@/components/atomic-design/atoms/texts/titleHome';
 import { ItemText } from '@/components/atomic-design/atoms/texts/itemText';
 import { RoundedButton } from '@/components/atomic-design/atoms/buttons/roundedButton';
 
 const HeroLayout = () => {
+  const router = useRouter();
+
+  const handleDiagnoseNowClick = () => {
+    router.push('/page-2');
+  };
+
   return (
     <section className="pt-40 pl-32 min-h-screen w-full flex flex-col relative ">
-        <div className="text-7xl">
-          <TitleHome>
-            <p className="pb-4">Welcome to</p>
-            <p>WWWMYPC</p>
-          </TitleHome>
-        </div>
-        <div className="pt-14 text-[#898786]">
+      <div className="text-7xl">
+        <TitleHome>
+          <p className="pb-4">Welcome to</p>
+          <p>WWWMYPC</p>
+        </TitleHome>
+      </div>
+      <div className="pt-14 text-[#898786]">
+        <ItemText>
+          <p>WWWMYPC is a comprehensive PC software and hardware</p>
+          <p> diagnosticator that utilizes a cutting-edge chatbot to help</p>
+          <p>users effortlessly identify and resolve their computer-related </p>
+        </ItemText>
+      </div>
+      <div className="space-x-7 pt-9">
+        <RoundedButton
+          backgroundColor="#032B4D"
+          textColor="#898786"
+          className="font-bold w-40 h-12 justify-center"
+          onClick={handleDiagnoseNowClick}
+        >
           <ItemText>
-            <p>WWWMYPC is a comprehensive PC software and hardware</p>
-            <p> diagnosticator that utilizes a cutting-edge chatbot to help</p>
-            <p>users effortlessly identify and resolve their computer-related </p>
+            <p>Diagnose Now</p>
           </ItemText>
-        </div>
-        <div className="space-x-7 pt-9">
-          <RoundedButton backgroundColor="#032B4D" textColor="#898786" className="font-bold w-40 h-12 justify-center">
-            <ItemText>
-              <p>Diagnose Now</p>
-            </ItemText>
-          </RoundedButton>
+        </RoundedButton>
 
-          <RoundedButton backgroundColor="#FFFFFF" textColor="#898786" className="font-bold w-40 h-12 justify-center">
-            <ItemText>
-              <p>Get Support</p>
-            </ItemText>
-          </RoundedButton>
-
-        </div>
+        <RoundedButton
+          backgroundColor="#FFFFFF"
+          textColor="#898786"
+          className="font-bold w-40 h-12 justify-center"
+        >
+          <ItemText>
+            <p>Get Support</p>
+          </ItemText>
+        </RoundedButton>
+      </div>
     </section>
   );
 };
 
-export { HeroLayout};
+export { HeroLayout };
